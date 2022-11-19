8de4d137d2d1d8c232e9303e362ce95d0b8a63dd
Kyuhan1230 <asdm159@naver.com>
2022-11-20 02:34:28 +0900

    Create Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="FaultDetect",
+      version="0.0.0",
+      description="Fault Detection/Identification/Reconstruction",
+      url="https://github.com/TechDas/FaultDetect.git",
+      author="Kyu Han, Seok",
+      author_email="asdm159@gmail.com",
+      zip_safe=False,
+      install_requires=[
+            'numpy==1.19.5'
+      ]
+)
