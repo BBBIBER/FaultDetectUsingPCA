bcafd94476a93e05ffa38d1074af1b17cc9a1fb9
Kyuhan,Seok <asdm159@gmail.com>
2023-01-11 21:31:52 +0900

    Upload FaultDetectAlgo

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name="FaultDetect",
-      version="0.0.1",
-      description="Fault Detection/Identification/Reconstruction",
-      url="https://github.com/TechDas/FaultDetect.git",
+      version="0.1",
+      description="Fault Detection, Identification and Reconstruction Using PCA(Principal Component Analysis)",
+      url="https://github.com/Kyuhan1230/FaultDetect",
       author="Kyu Han, Seok",
       author_email="asdm159@gmail.com",
       zip_safe=False,
